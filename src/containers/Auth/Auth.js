0ccfaf4581bfb2d72b252671d1ae8ebd978270c9
DavidSalami <wizdave97@gmail.com>
2019-04-07 16:10:38 +0100

    Created Authentication Component Auth.js

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import formSerialize from 'form-serialize';
+
+ class Auth extends Component {
+
+   render(){
+     return (
+
+     )
+   }
+ }
+ export default Auth;
