dba6f29b2d48d19ed41a5c92c8110730e891d6c0
DavidSalami <wizdave97@gmail.com>
2019-04-14 18:31:15 +0100

    Updates

---
+++ precache-manifest.d807bf2264b5c026485ec0973bb54d9c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1f4f73ef819cc57dd9b",
+    "url": "/inventory-checklist/static/css/5.09ab03e9.chunk.css"
+  },
+  {
+    "revision": "034ed436070f95624a26",
+    "url": "/inventory-checklist/static/css/main.48695cba.chunk.css"
+  },
+  {
+    "revision": "5c02ded33184d821aa1d",
+    "url": "/inventory-checklist/static/js/runtime~main.bf7a6db7.js"
+  },
+  {
+    "revision": "c9e7c7ddd9325727a359",
+    "url": "/inventory-checklist/static/js/2.6ebfa1f5.chunk.js"
+  },
+  {
+    "revision": "17fdc0535a0fcf1a3573ff13e2e90ad3",
+    "url": "/inventory-checklist/static/media/placeholder.17fdc053.png"
+  },
+  {
+    "revision": "8c86a3327fb7ecddbc8c",
+    "url": "/inventory-checklist/static/js/3.b39a1531.chunk.js"
+  },
+  {
+    "revision": "7f4c41ff6b33f1313e277af91a70f6f1",
+    "url": "/inventory-checklist/static/media/logo.7f4c41ff.svg"
+  },
+  {
+    "revision": "b1fe29789cf14b25b89f",
+    "url": "/inventory-checklist/static/js/4.f9d500cc.chunk.js"
+  },
+  {
+    "revision": "034ed436070f95624a26",
+    "url": "/inventory-checklist/static/js/main.68f3c946.chunk.js"
+  },
+  {
+    "revision": "f1f4f73ef819cc57dd9b",
+    "url": "/inventory-checklist/static/js/5.2e96d50b.chunk.js"
+  },
+  {
+    "revision": "38e88453f276ccda4c8b",
+    "url": "/inventory-checklist/static/js/7.2926543f.chunk.js"
+  },
+  {
+    "revision": "c7894fd918d22145f810",
+    "url": "/inventory-checklist/static/js/6.fe5c993b.chunk.js"
+  },
+  {
+    "revision": "38e88453f276ccda4c8b",
+    "url": "/inventory-checklist/static/css/7.09ab03e9.chunk.css"
+  },
+  {
+    "revision": "c7894fd918d22145f810",
+    "url": "/inventory-checklist/static/css/6.09ab03e9.chunk.css"
+  },
+  {
+    "revision": "b1fe29789cf14b25b89f",
+    "url": "/inventory-checklist/static/css/4.09ab03e9.chunk.css"
+  },
+  {
+    "revision": "8c86a3327fb7ecddbc8c",
+    "url": "/inventory-checklist/static/css/3.09ab03e9.chunk.css"
+  },
+  {
+    "revision": "db9c6352c7f4ca62c25d4a4ce9689818",
+    "url": "/inventory-checklist/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/inventory-checklist/precache-manifest.d807bf2264b5c026485ec0973bb54d9c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/inventory-checklist/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
